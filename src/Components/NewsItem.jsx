a701f22ec473803208c00262990a14e5dd17785f
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2025-03-05 14:21:20 +0530

    Update NewsItem.jsx

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import image from "../assets/News.png";
+import defaultImage from "../assets/News.png"; // Default image
 
 const NewsItem = ({ title, description, src, url }) => {
-  const validSrc = src || image;
+  const validSrc = src || defaultImage;
 
   return (
     <div className="card bg-dark text-light mb-3">
@@ -10,7 +10,7 @@ const NewsItem = ({ title, description, src, url }) => {
       <div className="card-body">
         <h5 className="card-title">{title ? title.slice(0, 50) : "No Title Available"}</h5>
         <p className="card-text">
-          {description ? description.slice(0, 90) : "Stay updated with our daily news feeds, bringing you the latest headlines."}
+          {description ? description.slice(0, 90) : "Stay updated with our daily news feeds."}
         </p>
         <a href={url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
           Read More
